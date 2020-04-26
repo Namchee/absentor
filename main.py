9cf931a0a4318eeb8dff3241fa315ca66278e552
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-04-26 16:08:04 +0700

    refactor: Moved 'on_ready' listener to cogs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from google.oauth2.service_account import Credentials
 from discord.ext.commands import Bot
-from discord import Game, Status
 from os import environ
 from dotenv import load_dotenv, find_dotenv
 from gspread import authorize
@@ -13,6 +12,7 @@ load_dotenv(find_dotenv())
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
+
 credentials = Credentials.from_service_account_file('credentials.json', scopes=scope)
 
 gc = authorize(credentials)
@@ -23,20 +23,8 @@ sheet = Sheet(spreadsheet, spreadsheet.get_worksheet(0))
 bot = Bot(command_prefix="!")
 bot.add_cog(AbsentorBot(bot, sheet))
 
-"""On connection, log all guilds connected to this bot instance
-    And change the status
-"""
-@bot.event
-async def on_ready():
-    print('Absentor is connected to these servers:')
-
-    for guild in bot.guilds:
-        print('{} ({})'.format(guild.name, guild.id))
-
-    await bot.change_presence(status=Status.online, activity=Game(name="Absensi Mahasiswa"))
-
 def main():
-    pass
+    bot.run(environ.get("DISCORD_TOKEN"))
 
 if __name__ == "__main__":
     main()
