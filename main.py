1b44528defed2ed5e41a56df556c0f64c4675415
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-05-07 14:29:17 +0700

    deploy: Changed how Google OAuth is handled

--- main.py
+++ main.py
@@ -5,13 +5,27 @@ from dotenv import load_dotenv, find_dotenv
 from gspread import authorize
 from src.bot import AbsentorBot
 from src.sheet import Sheet
+from datetime import date
 
 load_dotenv(find_dotenv())
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-credentials = Credentials.from_service_account_file('credentials.json', scopes=scope)
+credentials_info = {
+    'type': environ.get('TYPE'),
+    'project_id': environ.get('PROJECT_ID'),
+    'private_key_id': environ.get('PRIVATE_KEY_ID'),
+    'private_key': environ.get('PRIVATE_KEY'),
+    'client_email': environ.get('CLIENT_EMAIL'),
+    'client_id': environ.get('CLIENT_ID'),
+    'auth_uri': environ.get('AUTH_URI'),
+    'token_uri': environ.get('TOKEN_URI'),
+    'auth_provider_x509_cert_url': environ.get('AUTH_PROVIDER_X509_CERT_URL'),
+    'client_x509_cert_url': environ.get('CLIENT_X509_CERT_URL')
+}
+
+credentials = Credentials.from_service_account_info(credentials_info, scopes=scope)
 
 gc = authorize(credentials)
 spreadsheet = gc.open_by_key(environ.get("SHEET_ID"))
@@ -22,7 +36,8 @@ bot = Bot(command_prefix="!")
 bot.add_cog(AbsentorBot(bot, sheet))
 
 def main():
-    bot.run(environ.get("DISCORD_TOKEN"))
+    # bot.run(environ.get("DISCORD_TOKEN"))
+    sheet.clear_absen(date.today())
 
 if __name__ == "__main__":
     main()
